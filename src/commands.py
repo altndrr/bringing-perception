954f433e2b61be8d57ecacbf1c389f14812b66f9
Alessandro Conti <alessandro.conti.97@gmail.com>
2022-05-11 13:48:43 +0200

    Update .gitignore

--- src/commands.py
+++ src/commands.py
@@ -1,15 +1,26 @@
 """Commands of the cli."""
 
+import json
 import os
 import random
 import shutil
 import zipfile
+from glob import glob
 
 import gdown
 import numpy as np
+import requests as r
 import torch
+import torchvision.transforms as T
+from PIL import Image
 from torchvision.datasets import ImageFolder
+from torchvision.models.resnet import resnet50
 from tqdm import tqdm
+
+GROUP_NAME = "group_3"
+SUBMISSION_URL = "http://coruscant.disi.unitn.it:3001/results/"
+
+BATCH_SIZE = 64
 
 
 def download_datasets(*args, **kwargs):
@@ -49,36 +60,21 @@ def prepare_data(*args, **kwargs):
     """Prepare the dataset to generate the query, the gallery and the triplets."""
     dataset_dir = kwargs.get("<dataset_dir>")
     parent_dir = os.path.dirname(dataset_dir)
-    num_triplets = int(kwargs.get("--num", 1000))
+    num_triplets = int(kwargs.get("--num") or 1000)
 
     assert dataset_dir is not None
 
     ds = ImageFolder(dataset_dir)
-    sizes = (int(len(ds) * 0.8), len(ds) - int(len(ds) * 0.8))
-    train_set, val_set = torch.utils.data.random_split(ds, sizes)
 
-    val_targets = np.array(ds.imgs)[:, 0][val_set.indices].tolist()
-    random.shuffle(val_targets)
-    val_query = val_targets[: int(len(val_targets) * 0.2)]
-    val_gallery = val_targets[int(len(val_targets) * 0.2) :]
-    with open(os.path.join(parent_dir, "query.txt"), "w", encoding="utf-8") as f:
-        f.writelines("\n".join(val_query))
-    with open(os.path.join(parent_dir, "gallery.txt"), "w", encoding="utf-8") as f:
-        f.writelines("\n".join(val_gallery))
-
-    train_targets = np.array(ds.targets)[train_set.indices]
-    class_idx, num_samples = np.unique(train_targets, return_counts=True)
+    targets = np.array(ds.targets)
+    class_idx, num_samples = np.unique(targets, return_counts=True)
     positive_classes = class_idx[num_samples > 1]
 
     triplets = []
     for _ in tqdm(range(num_triplets)):
         class_idx = random.choice(positive_classes)
-        positives = [
-            ds.imgs[i][0] for i in train_set.indices if ds.targets[i] == class_idx
-        ]
-        negatives = [
-            ds.imgs[i][0] for i in train_set.indices if ds.targets[i] != class_idx
-        ]
+        positives = [path for path, target in ds.imgs if target == class_idx]
+        negatives = [path for path, target in ds.imgs if target != class_idx]
 
         assert len(positives) > 1
 
@@ -96,3 +92,95 @@ def prepare_data(*args, **kwargs):
 
     with open(os.path.join(parent_dir, "triplets.txt"), "w", encoding="utf-8") as f:
         f.writelines(triplets)
+
+
+def test_gallery(*args, **kwargs):
+    """Test a model on a query-gallery system."""
+    checkpoint = kwargs.get("<model>")
+    query_dir = kwargs.get("<query_dir>") or "./datasets/lfw/test/query/"
+    gallery_dir = kwargs.get("<gallery_dir>") or "./datasets/lfw/test/gallery/"
+
+    assert checkpoint is not None
+
+    model = resnet50(num_classes=8631)
+    weights = torch.load(checkpoint, map_location="cpu")
+    model.load_state_dict(weights, strict=True)
+    model.fc = torch.nn.Identity()
+
+    query_images = glob(os.path.join(query_dir, "*"))
+    gallery_images = glob(os.path.join(gallery_dir, "*"))
+
+    query_embeddings = _forward_step(model, query_images)
+    gallery_embeddings = _forward_step(model, gallery_images)
+
+    results = {}
+    gallery_names = np.array([os.path.basename(image) for image in gallery_images])
+    for i, embedding in enumerate(query_embeddings):
+        query_name = os.path.basename(query_images[i])
+        distances = (gallery_embeddings - embedding).norm(dim=1)
+        top_10 = gallery_names[distances.topk(10, largest=False)[1].to(int)]
+
+        results[query_name] = top_10.tolist()
+
+    if kwargs.get("--submit"):
+        _submit(results)
+
+
+def _forward_step(model, images):
+    model.eval()
+    model.to("cuda")
+
+    num_batches = int(len(images) / BATCH_SIZE) + 1
+    batch_start = lambda x: x * BATCH_SIZE
+    batch_end = lambda x: (x * BATCH_SIZE) + BATCH_SIZE
+    total_embeddings = torch.Tensor()
+    for i in tqdm(range(num_batches), desc="forwarding...", total=num_batches):
+        batch = _read_images(images[batch_start(i) : batch_end(i)]).to("cuda")
+        embeddings = model(batch).detach().cpu()
+        total_embeddings = torch.cat((total_embeddings, embeddings), dim=0)
+
+    return total_embeddings
+
+
+def _fixed_image_standardization(image):
+    processed_tensor = (image - 127.5) / 128.0
+    return processed_tensor
+
+
+def _read_images(images):
+    tensors = []
+    for image in images:
+        if isinstance(image, str):
+            if not os.path.exists(image) or not os.path.isfile(image):
+                raise FileNotFoundError(f"{image} is an invalid image path")
+
+            image = Image.open(image)
+
+        # Convert to RGB to fix the number of channels to three.
+        image = image.convert("RGB")
+
+        transformations = T.Compose([np.float32, T.ToTensor(), T.Resize((160, 160))])
+        tensors.append(transformations(image))
+
+    tensors = [_fixed_image_standardization(tensor) for tensor in tensors]
+    return torch.stack(tensors)
+
+
+def _submit(results):
+    """
+    Submit a json file to the system for evaluation.
+    """
+    assert isinstance(results, dict) and results is not None
+
+    data = {}
+    data["groupname"] = GROUP_NAME
+    data["images"] = results
+
+    res = json.dumps(data)
+
+    response = r.post(SUBMISSION_URL, res)
+    try:
+        result = json.loads(response.text)
+        print(f"accuracy is {result['results']}")
+    except json.JSONDecodeError:
+        print(f"ERROR: {response.text}")
--- src/main.py
+++ src/main.py
@@ -4,25 +4,29 @@ src
 Usage:
     src download <dataset>
     src prepare <dataset_dir> [--num VAL]
+    src test <model> [<query_dir>] [<gallery_dir>] [--submit]
     src -h | --help
     src --version
 
 Options:
+    --submit                    Submit the results or not.
     -n VAL --num=VAL            Num of triplets to generate
     -h --help                   Show the help screen
     --version                   Print the current version
 """
 
 import random
+import sys
+from warnings import warn
 
 import numpy as np
 import torch
 from docopt import docopt
 
 from src import __version__
-from src.commands import download_datasets, prepare_data
+from src.commands import download_datasets, prepare_data, test_gallery
 
-COMMANDS = {"download": download_datasets, "prepare": prepare_data}
+COMMANDS = {"download": download_datasets, "prepare": prepare_data, "test": test_gallery}
 
 
 def main():
@@ -38,3 +42,6 @@ def main():
         command = COMMANDS.get(key)
         if options[key] is True and command is not None:
             command(**options)
+            sys.exit()
+
+    warn("command not recognised")
